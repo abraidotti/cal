c31456d2c0309eb0ee2cc5e0c7de55d3850f06ea
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-01 17:15:37 -0500

    add events and calendars controllers

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,21 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'events/show'
+
+  get 'events/new'
+
+  get 'events/edit'
+
+  get 'events/create'
+
+  get 'events/update'
+
+  get 'events/destroy'
+
+  get 'calendars/index'
+
   devise_for :users
   root to: "calendar#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
