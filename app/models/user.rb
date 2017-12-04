8b08a6429683633a1c3ae36baf05a70a2d05aa96
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-04 14:12:52 -0500

    add entity relationships to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  has_one :calendar
+  has_many :events
+  after_create :build_calendar
+
+  def build_calendar
+    Calendar.create(user_id: self.id)
+    # Associations must be defined correctly for this syntax, avoids using ID's directly.
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
