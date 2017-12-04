de834de13c0624c75a9cf170303d31e73c693b40
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-04 11:39:23 -0500

    add name, desc, location, start_time, and end_time to events

--- db/migrate/20171201220819_create_events.rb
+++ db/migrate/20171201220819_create_events.rb
@@ -1,7 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
-
+      t.string :name
+      t.string :description
+      t.string :location
+      t.datetime :start_time
+      t.datetime :end_time
       t.timestamps
     end
   end
