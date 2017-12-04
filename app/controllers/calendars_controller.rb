d2d9263470f43b7ca474ccb03435bad4ad3690d3
Nicole W <nicolemairead4@gmail.com>
2017-12-04 14:02:39 -0500

    add user id but also dont merge this

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,6 +2,11 @@ class CalendarsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @calendar = Calendar.where(user_id: current_user.id)
     @events = Event.all
+  end
+
+  def show
+    @events = Events.all
   end
 end
