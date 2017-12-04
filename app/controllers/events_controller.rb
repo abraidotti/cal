3f8344800fd685680cf01ee6af97bff569628f17
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-04 11:50:03 -0500

    add new event form and calendar display

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,17 +5,30 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def edit
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      redirect_to new_event_path, notice: @event.errors.full_messages.last
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :start_time, :end_time)
   end
 end
