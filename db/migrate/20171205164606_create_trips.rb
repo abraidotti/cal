b6ac72bc057413a1d041e97574354900f4b49e1c
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-05 14:26:52 -0500

    add data columns to trips model

--- db/migrate/20171205164606_create_trips.rb
+++ db/migrate/20171205164606_create_trips.rb
@@ -1,7 +1,13 @@
 class CreateTrips < ActiveRecord::Migration[5.1]
   def change
     create_table :trips do |t|
-
+      t.string :name
+      t.string :description
+      t.string :location
+      t.decimal :cost, precision: 10, scale: 2
+      t.integer :duration
+      t.datetime :start_time
+      t.datetime :end_time
       t.timestamps
     end
   end
