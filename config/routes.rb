48f748dc7be8998cb2669aea9d73e631ecefa439
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-06 14:31:45 -0500

    add events to trips is now doable

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "calendars#index"
+  post '/trips/:id', to: 'trips#remove'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
