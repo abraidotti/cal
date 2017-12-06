5adb90a85e332f631cfe34ea166dc4b327548b3e
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-06 14:58:29 -0500

    change hardcoded addresses to prep for geocoding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-$locations = ['Philadelphia', 'New York', 'Shanghai', 'San Francisco', 'Miami']
+$locations = ['1419 South Mole St, Philadelphia, PA', '2314 East Harold St, Philadelphia, PA', '1710 Naudain St, Philadelphia, PA', '1502 South Broad St, Philadelphia, PA', '826 Morris St, Philadelphia, PA']
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
