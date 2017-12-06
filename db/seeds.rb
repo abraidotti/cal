03faca539dd5c8f3b043351b74fbac3e54c69abf
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-06 12:15:45 -0500

    add broken seed users and trips

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Require 'faker'
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -13,13 +13,30 @@ test_trip = Trip.create(name: "A night in Philly", description: "a nutritional g
 
 3.times do
   user = User.create(
-    username: Faker::Internet.email,
+    email: Faker::Internet.email,
     password: '123456',
     )
     2.times do
       Trip.create(
         name: Faker::Coffee.blend_name,
-        description: "Faker::RickAndMorty.quote"
+        description: Faker::RickAndMorty.quote,
+        location: "Philly",
+        start_time: Time.now,
+        end_time: Time.now,
+        duration: 4,
+        cost: 40,
+        user_id: user.id
+      )
+    end
+    3.times do
+      user.trips << Event.create(
+        name: Faker::Coffee.blend_name,
+        description: Faker::Coffee.notes,
+        location: Faker::Space.planet,
+        start_time: Time.now,
+        end_time: Time.now,
+        duration: 3,
+        cost: 5,
         user_id: user.id
       )
     end
