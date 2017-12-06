9279fcb6966a25f969d37f03e5b8142a8f341564
Nicole W <nicolemairead4@gmail.com>
2017-12-06 17:25:28 -0500

    add event and trip validation on names and descriptions

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :trips
+  validates :name, presence: true
+  validates :description, presence: true, length: {minimum: 5, maximum: 140}
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ApplicationRecord
   has_and_belongs_to_many :events
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true, length: {minimum: 5, maximum: 140}
 end
