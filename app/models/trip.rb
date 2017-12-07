047b88e8917cf75ea778d9de41c4e2d5c2571227
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-07 00:43:03 -0500

    add changes to how duration is displayed

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,5 @@ class Trip < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: {minimum: 1, maximum: 50}
   validates :description, presence: true, length: {minimum: 5, maximum: 140}
+  validates :duration, presence: true, length: {minimum: 1, maximum: 240, too_long: "%{count / 24} days is the maximum allowed for an event. Try breaking your event up into smaller ones." }
 end
