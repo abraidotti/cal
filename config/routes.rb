05bbe1ff7f92270803e42cf5dc771347ab6bd6de
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-07 16:42:43 -0500

    add users index and shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
+  devise_for :users
   resources :events
   resources :trips
+  root to: "calendars#index"
 
+  get 'users/index', to: 'users#index'
+  get 'users/show/:id', to: 'users#show'
   get 'calendars/index'
   get 'calendars/show'
 
-  devise_for :users
-  root to: "calendars#index"
   post '/trips/:id', to: 'trips#remove'
   post '/events/:id', to: 'trips#update'
 
