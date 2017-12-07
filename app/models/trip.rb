8410f069145593a41a144ca599d987165139c3a2
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-06 23:57:11 -0500

    add name length validations to user name, lname, events, and trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   has_and_belongs_to_many :events
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 1, maximum: 50}
   validates :description, presence: true, length: {minimum: 5, maximum: 140}
 end
