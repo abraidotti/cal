eae5bf293cac3b799c2e7fa6ac9eee66320a76c0
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-07 14:34:31 -0500

    fix fname and lname save on user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 $locations = ['1419 South Mole St, Philadelphia, PA', '2314 East Harold St, Philadelphia, PA', '1710 Naudain St, Philadelphia, PA', '1502 South Broad St, Philadelphia, PA', '826 Morris St, Philadelphia, PA']
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:fname, :lname]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
+
   protect_from_forgery with: :exception
 
   private
