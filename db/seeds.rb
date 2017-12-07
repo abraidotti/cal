1985d3bee8fc6420671af4fe1931f1ea5f1cd8e6
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2017-12-06 23:56:53 -0500

    fix syntax error in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,42 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-test_user = User.create(email: "test@test.com", fname: "Testy", lname: "Testaverde", password: "123456")
+test_user = User.create(
+  email: "test@test.com",
+  fname: "Testy",
+  lname: "Testaverde",
+  password: "123456"
+)
 
 test_user.events << Event.create(
-  name: Faker::Food.metric_measurement + " " + user.email,
+  name: Faker::Food.metric_measurement,
   description: Faker::Coffee.notes,
   location: "824 Morris St, Philadelphia, PA",
   start_time: Time.now + 1.hours,
   end_time: Time.now + 3.hours,
   duration: 3,
   cost: 5,
-  user_id: user.id
+  user_id: test_user.id
 )
-
-test_user << Trip.create(name: "A night in Philly", description: "a nutritional gauntlet", location: "Philly", start_time: Time.now, end_time: Time.now + 8.hours, duration: 4, cost: 40, user_id: test_user.id)
-
+test_user.trips << Trip.create(
+  name: "A night in Philly",
+  description: "a nutritional gauntlet",
+  location: "741 Federal St, Philadelphia, PA",
+  start_time: Time.now,
+  end_time: Time.now + 8.hours,
+  duration: 4,
+  cost: 40,
+  user_id: test_user.id
+)
 test_user.trips.last.events << Event.create(
-  name: Faker::Coffee.blend_name + " " + user.email,
+  name: Faker::Coffee.blend_name,
   description: Faker::Hipster.sentence(3, true),
   location: "1502 South Broad St, Philadelphia, PA",
   start_time: Time.now + 1.hours,
   end_time: Time.now + 3.hours,
   duration: 3,
   cost: 5,
-  user_id: user.id
+  user_id: test_user.id
 )
 
 user = User.create(
@@ -38,40 +50,40 @@ user = User.create(
   fname: Faker::Ancient.primordial,
   lname: Faker::Food.spice,
   password: '123456',
+)
+3.times do
+  user.events << Event.create(
+    name: Faker::Coffee.blend_name,
+    description: Faker::Coffee.notes,
+    location: "2314 East Harold St, Philadelphia, PA",
+    start_time: Time.now + 1.hours,
+    end_time: Time.now + 3.hours,
+    duration: 3,
+    cost: 5,
+    user_id: user.id
   )
-  3.times do
-    user.events << Event.create(
-      name: Faker::Coffee.blend_name + " " + user.email,
-      description: Faker::Coffee.notes,
-      location: "2314 East Harold St, Philadelphia, PA",
-      start_time: Time.now + 1.hours,
-      end_time: Time.now + 3.hours,
-      duration: 3,
-      cost: 5,
-      user_id: user.id
-    )
-  end
-  3.times do
-      user.trips << Trip.create(
-        name: Faker::Coffee.blend_name + " " + user.email,
-        description: Faker::RickAndMorty.quote,
-        location: "145 Emily St, Philadelphia, PA",
-        start_time: Time.now,
-        end_time: Time.now + 9.hours,
-        duration: 4,
-        cost: 40,
-        user_id: user.id
-      )
-    end
-    3.times do
-      user.trips.last.events << Event.create(
-        name: Faker::Space.planet + " " + user.email,
-        description: Faker::Coffee.notes,
-        location: "1600 East Cherry St, Philadelphia, PA",
-        start_time: Time.now + 1.hours,
-        end_time: Time.now + 3.hours,
-        duration: 3,
-        cost: 5,
-        user_id: user.id
-      )
-    end
+end
+3.times do
+  user.trips << Trip.create(
+    name: Faker::Coffee.blend_name,
+    description: Faker::RickAndMorty.quote,
+    location: "145 Emily St, Philadelphia, PA",
+    start_time: Time.now,
+    end_time: Time.now + 9.hours,
+    duration: 4,
+    cost: 40,
+    user_id: user.id
+  )
+end
+3.times do
+  user.trips.last.events << Event.create(
+    name: Faker::Space.planet,
+    description: Faker::Coffee.notes,
+    location: "1600 East Cherry St, Philadelphia, PA",
+    start_time: Time.now + 1.hours,
+    end_time: Time.now + 3.hours,
+    duration: 3,
+    cost: 5,
+    user_id: user.id
+  )
+end
