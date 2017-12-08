15736e1a93dcf716642f78c7b9d94c668bff2f23
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2017-12-08 14:13:30 -0500

    Infowindow added to t trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,6 +12,7 @@ class TripsController < ApplicationController
     @markers = Gmaps4rails.build_markers(@events) do |event, marker|
       marker.lat event.latitude
       marker.lng event.longitude
+      marker.infowindow event.description
     end
   end
 
